5b4a06b4a4df61ccd170c1a753738539437e2e94
Josh Smith <josmith@csumb.edu>
2017-02-14 03:54:19 +0000

    first

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 import os
-import flask
+import flask, flask_socketio
+
 
 app = flask.Flask(__name__)
 
+socketio = flask_socketio.SocketIO(app)
+
 @app.route('/')
 def hello():
-    return flask.render_template('index.html')
-
-app.run(
+   return flask.render_template('index.html')
+    
+@socketio.on('connect')
+def on_connect():
+   print 'Someone connected!------------------------------------'
+    
+socketio.run(
+    app,
     host=os.getenv('IP', '0.0.0.0'),
-    port=int(os.getenv('PORT', 8080)),
-    debug=True
+    port=int(os.getenv('PORT', 8080))
 )
---
+++ scripts/MyFavoriteAnimal.js
@@ -0,0 +1,8 @@
+import * as React from 'react';
+
+export class MyFavoriteAnimal
+extends React.Component {
+    render() {
+        return <li>{this.props.animal}</li>;
+    }
+}
