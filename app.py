975f2d2b881175a59e7de2d1736d9d8d427643f1
Josh Smith <josmith@csumb.edu>
2017-02-20 08:04:28 +0000

    bug fix help bot

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def handle_my_custom_event(data):
              socketio.emit('bot:message', about, broadcast=True, include_self=True)
          elif(cmp(the_text[4:len(the_text) -1], 'about')):
              socketio.sleep(seconds=0.1)
-             socketio.emit('bot:message', about, broadcast=True, include_self=True)
+             socketio.emit('bot:message', help, broadcast=True, include_self=True)
      
 
     
