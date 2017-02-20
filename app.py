0e3013802c456317a1f0c955dbc5db21e28b6b61
Josh Smith <josmith@csumb.edu>
2017-02-20 08:03:16 +0000

    got help bot working

--- app.py
+++ app.py
@@ -39,10 +39,10 @@ def handle_my_custom_event(data):
      the_text = json.dumps(data['text'])
 #    print the_text[1:3]
      if(the_text[1:3] == '!!'):
-         if(the_text[4:len(the_text) -1] == 'about'):
+         if(cmp(the_text[4:len(the_text) -1], 'about')):
              socketio.sleep(seconds=0.1)
              socketio.emit('bot:message', about, broadcast=True, include_self=True)
-         elif(the_text[4:len(the_text) -1] == 'help'):
+         elif(cmp(the_text[4:len(the_text) -1], 'about')):
              socketio.sleep(seconds=0.1)
              socketio.emit('bot:message', about, broadcast=True, include_self=True)
      
