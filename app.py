f6639d0ce368522c9cb95d45c64f7e943b0b3161
Josh Smith <josmith@csumb.edu>
2017-02-20 06:32:32 +0000

    bot didn't work starting over

--- app.py
+++ app.py
@@ -31,43 +31,8 @@ def on_connect():
    
 @socketio.on('send:message')
 def handle_my_custom_event(data):
-    
      if request.sid in socket_ids:
-        text_list = TextBlob(data['text'])
-        the_list = text_list.words
-        
-        if(the_list[0] == '!!'):
-           if(the_list[1] == 'about'): 
-               socketio.emit('message:bot', about, broadcast=True, include_self=True)
-           elif(the_list[1] == 'help'):
-               socketio.emit('message:bot', help, broadcast=True, include_self=True)
-           elif(the_list[1] == 'say'):
-               the_list.pop(0)
-               the_list.pop(0)
-               the_data = ', '.join(the_list).translate(to='es')
-               socketio.emit('message:bot', the_data, broadcast=True, include_self=True)
-           elif(the_list[1] == 'spanish'):
-               the_list.pop(0)
-               the_list.pop(0)
-               the_data = ', '.join(the_list).translate(to='es')
-               socketio.emit('message:bot', the_data, broadcast=True, include_self=True)
-           elif(the_list[1] == 'pic'):
-               
-               image_url = "https://media.gettyimages.com/photos/-id"
-
-               url_test = 'https://api.gettyimages.com/v3/search/images?fields=id,title,referral_destinations&sort_order=best&phrase=surfing'
-               headers = {'Api-Key' : 'hns6ee6gtcnf9frgssyefkyg'}
-               r = {'fields' : 'comp'}
-               res = requests.get(url_test, headers=headers)
-               images_url = res.json()
-               rand_num2 = random.randint(0, len(images_url['images']) - 1)
-               image_url = images_url['images'][rand_num2]['display_sizes'][0]['uri']
-               
-               socketio.emit('message:bot', image_url, broadcast=True, include_self=True)
-           else:
-               socketio.emit('message:bot', dont_recon, broadcast=True, include_self=True)
-        else:
-           socketio.emit('send:message', data, broadcast=True, include_self=False)
+        socketio.emit('send:message', data, broadcast=True, include_self=False)
     
 @socketio.on('facebook:athenticate', namespace='/')
 def test_connect_facebook(data):
