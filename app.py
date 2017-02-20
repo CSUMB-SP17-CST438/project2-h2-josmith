5b45472908bb6d502d491b4f8dd897ff26bd8415
Josh Smith <josmith@csumb.edu>
2017-02-20 09:32:54 +0000

    bug fix help bot

--- app.py
+++ app.py
@@ -35,10 +35,10 @@ def handle_my_custom_event(data):
      if request.sid in socket_ids:
          socketio.emit('send:message', data, broadcast=True, include_self=False)
      
-     the_text = str(json.dumps(data['text'], ensure_ascii=False))
-     the_text2 = str(json.dumps(data['text'], ensure_ascii=True))
-     print the_text is str
-     print the_text2 is str
+     the_text = json.dumps(data['text'], ensure_ascii=False)
+     the_text2 = json.dumps(data['text'], ensure_ascii=True)
+     print the_text == str
+     print the_text2 == str
     #  the_str = str(the_text[4:len(the_text) -1])
     #  print the_text[4:len(the_text) -1]
     #  print the_text[4:len(the_text) -1] is 'about'
