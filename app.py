114f5c58d6e39810f2ca2cc6d0261f8133c7538a
Josh Smith <josmith@csumb.edu>
2017-03-02 23:21:35 +0000

    refactored bot code for testing

--- app.py
+++ app.py
@@ -32,9 +32,6 @@ socketio = SocketIO(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://smitjb45:Goldfish83-@localhost/postgres'
 db = flask_sqlalchemy.SQLAlchemy(app)
 
-about = 'This is a chat app that was build in CSUMBs software engineering class in two weeks'
-help = 'The options are about, help, say: !! say <words to say>, mario, softkitty, yoshi, and kenny'
-dont_recon = 'Sorry, I dont uderstand that command'
 
 import models
 
@@ -73,7 +70,8 @@ def handle_my_custom_event(data):
          
          socketio.sleep(seconds=0.1)
          socketio.emit('send:message', data, broadcast=True, include_self=False)
-         bot(data)
+         response = bot(data)
+         socketio.emit('bot:message', response, broadcast=True, include_self=True)
     
 @socketio.on('facebook:athenticate', namespace='/')
 def test_connect_facebook(data):
@@ -99,33 +97,35 @@ def bot(data):
           
      if(the_text[0:2] == '!!'):
         if( the_text[3:len(the_text)] == "about"):
-            
-            socketio.emit('bot:message', about, broadcast=True, include_self=True)
+            return 'This is a chat app that was build in CSUMBs software engineering class in two weeks'
+            # socketio.emit('bot:message', about, broadcast=True, include_self=True)
         elif( the_text[3:len(the_text)] == "help"):
-            socketio.sleep(seconds=0.1)
-            socketio.emit('bot:message', help, broadcast=True, include_self=True)
+            return 'The options are about, help, say: !! say <words to say>, mario, softkitty, yoshi, and kenny'
+            # socketio.emit('bot:message', help, broadcast=True, include_self=True)
         elif( the_text[3:6] == "say"):
-            socketio.sleep(seconds=0.1)
-            socketio.emit('bot:message', the_text[7:len(the_text)], broadcast=True, include_self=True)
+            return the_text[7:len(the_text)]
+            # socketio.emit('bot:message', the_text[7:len(the_text)], broadcast=True, include_self=True)
         elif( the_text[3:len(the_text)] == "mario"):
             socketio.sleep(seconds=0.1)
-            socketio.emit('bot:message', mario, broadcast=True, include_self=True)
+            return mario
+            # socketio.emit('bot:message', mario, broadcast=True, include_self=True)
         elif( the_text[3:len(the_text)] == "softkitty"):
-            socketio.sleep(seconds=0.1)
-            socketio.emit('bot:message', softkitty, broadcast=True, include_self=True)
+            return softkitty
+            # socketio.emit('bot:message', softkitty, broadcast=True, include_self=True)
         elif( the_text[3:len(the_text)] == "yoshi"):
-            socketio.sleep(seconds=0.1)
-            socketio.emit('bot:message', yoshi, broadcast=True, include_self=True)
+            return yoshi
+            # socketio.emit('bot:message', yoshi, broadcast=True, include_self=True)
         elif( the_text[3:len(the_text)] == "kenny"):
-            socketio.sleep(seconds=0.1)
-            socketio.emit('bot:message', kenny, broadcast=True, include_self=True)
+            return kenny
+            # socketio.emit('bot:message', kenny, broadcast=True, include_self=True)
         elif( the_text[3:7] == "text"):
             client = middleware.TwilioRestClient(account_sid,auth_token)
             message = client.messages.create(to="+18314285108", from_="+18312010628",
                                      body=the_text[8:len(the_text)])
+            return "Text sent"
         else:
-         socketio.sleep(seconds=0.1)
-         socketio.emit('bot:message', dont_recon, broadcast=True, include_self=True)
+         return 'Sorry, I dont uderstand that command'
+        #  socketio.emit('bot:message', dont_recon, broadcast=True, include_self=True)
 
 if __name__ == '__main__':
     print db
